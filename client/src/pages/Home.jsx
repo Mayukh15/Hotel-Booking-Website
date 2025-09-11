ced0ba7eee715617f1f7948a57ac0aafcb673478
Mayukh Adhikari <mayukhadhikari7@gmail.com>
2025-09-11 20:26:52 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeatureDestination from '../components/FeatureDestination'
+import Exclusiveoffers from '../components/Exclusiveoffers'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeatureDestination/>
+      <Exclusiveoffers/>
+      <Testimonial/>
+      <NewsLetter/>
+    </>
+  )
+}
+
+export default Home
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import "dotenv/config"
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebHooks from "./controllers/ClerkWebHooks.js";
+
+connectDB()
+
+const app=express();
+app.use(cors());
+
+
+//middleware-get all data via json methods
+app.use(express.json)
+app.use(clerkMiddleware())
+
+//API to listen to Clerk WebHooks 
+app.use("/api/clerk",clerkWebHooks);
+
+app.get('/',(req,res)=>res.send("Api is working fine "));
+const PORT=process.env.port || 3000;
+ 
+app.listen(PORT,()=>console.log(`server is running on the port ${PORT}`));
